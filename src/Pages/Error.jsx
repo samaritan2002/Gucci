1f7d09f23da26e6a4235234c3c5cf132006518d1
samaritan2002 <anushapoojary392@gmail.com>
2025-03-01 11:55:14 +0530

    updated error

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Error() {
   return <div>
-    
-    <img src="" alt=""/>
+    <img src="https://cdn.dribbble.com/userupload/24450589/file/
+     original-7a69eb5b87401ce59325c3291535aebc.gif" alt=""/>
    
   </div>;
 }
